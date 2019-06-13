75f33150975bf71de4776b6504a71eba698c9b1e
John Rihacek <jrihacek@gmail.com>
2019-06-12 22:03:53 -0400

    get POST working

--- ping.py
+++ ping.py
@@ -1,22 +1,18 @@
-import subprocess, os, re, json, urllib.request, secrets, datetime
+import subprocess, os, re, json, urllib.request, secrets
+from datetime import datetime
+import requests #todo: ditch urllib stuff
 
 #call web page to get system info:
-urlString = "https://uptime.rihaceks.com/ping/list/" + secrets.pinglist
-webCall = urllib.request.urlopen(urlString)
+url = "https://uptime.rihaceks.com"
+pingString = url + "/ping/list/" + secrets.pinglist
+webCall = urllib.request.urlopen(pingString)
 listData = webCall.read()
 encoding = webCall.info().get_content_charset('utf-8')
 jsonList = json.loads(listData.decode(encoding))
 
-#temporary for debugging off network:
-#jsonList = json.loads('{"systems":[{"address":"127.0.0.42","description":"Local whatever on ESXI","parent":4,"pingtype":"PING","systemID":4,"systemName":"ESXi"},{"address":"127.0.0.10","description":"Local movie-streaming service","parent":4,"pingtype":"PING","systemID":5,"systemName":"Plex"}]}')
-
-#"ping" each system in the response
 pingParam = '-n' if os.name == 'nt' else '-c' #account for different ping parameters in Windows/Linux
 
-junkpassword = secrets.pinglist
-
 for system in jsonList["systems"]:
-    #password to post, systemid, statusid, calltime,[ and duration]
 
     thisSystem = system["systemID"]
     thisTime = datetime.now() #might have to format this later
@@ -24,7 +20,9 @@ for system in jsonList["systems"]:
         res = subprocess.check_output(['ping', pingParam, '1', system["address"]])
         failures = ['Received = 0', 'TTL expired in transit', 'host unreachable']
         if any(x in res.decode('utf-8') for x in failures):
-            raise Exception
+            print(system["systemName"] + ": todo - log this error.")
+            thisStatus = '6'
+            thisDuration = 0
         else:
             ms = re.search('Average = (.*)ms', res.decode('utf-8'))
             print(system["systemName"] + ": " + ms.group(1) + "ms")
@@ -35,22 +33,15 @@ for system in jsonList["systems"]:
     except subprocess.CalledProcessError as e:        
         print(system["systemName"] + ": todo - log this error.")
         thisStatus = '6'
+        thisDuration = 0
 
-    #do our post here like so
-    # $postParams = @{once='thisone';twice='thatone'}
-    # curl http://127.0.0.1:5000/responses/1 -Method POST -Body $postParams
-
-
-
-print("****************************************************") #debug separator
-'''
-for service in ["www.google.com","127.0.0.2"]:
-    try:
-        res = subprocess.check_output(['ping', pingParam, '1', service])
-        ms = re.search('Average = (.*)ms', res.decode('utf-8'))
-        print(service + ": " + ms.group(1) + "ms")
-    except subprocess.CalledProcessError as e:
-        #do failed ping stuff
-        print(service + ": No response.")
-'''
-
+    payload = { 'pw' : secrets.pinglist,
+                'st' : thisStatus,
+                'time' : thisTime,
+                'dur' : thisDuration } 
+    url = "http://127.0.0.1:5000" #debugging
+    postURL = url + "/responses/" + str(thisSystem)
+    res = requests.post(postURL, data=payload)
+    
+    print(res.text)
+    print('* * * * * *')
