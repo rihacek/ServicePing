7e3dbfdf59a8356206cf945ee2e9d5d24c32676c
John Rihacek <jrihacek@gmail.com>
2019-06-30 14:42:38 -0400

    correct ping response text for linux OS

--- ping.py
+++ ping.py
@@ -3,8 +3,8 @@ from datetime import datetime
 import requests #todo: ditch urllib stuff
 
 #call web page to get system info:
-url = "http://127.0.0.1:5000" #debugging
-#url = "https://uptime.rihaceks.com"
+# get these into config, dummy. url = "http://127.0.0.1:5000" #debugging
+url = "https://uptime.rihaceks.com"
 pingString = url + "/ping/list/" + secrets.pinglist
 webCall = urllib.request.urlopen(pingString)
 listData = webCall.read()
@@ -16,24 +16,21 @@ pingParam = '-n' if os.name == 'nt' else '-c' #account for different ping parame
 for system in jsonList["systems"]:
 
     thisSystem = system["systemID"]
-    thisTime = datetime.now() #might have to format this later
+    thisTime = datetime.now() 
     try:
         res = subprocess.check_output(['ping', pingParam, '1', system["address"]])
         failures = ['Received = 0', 'TTL expired in transit', 'host unreachable']
         if any(x in res.decode('utf-8') for x in failures):
-            print(system["systemName"] + ": todo - log this error.")
-            thisStatus = '6'
+            thisStatus = '2'
             thisDuration = 0
         else:
-            ms = re.search('Average = (.*)ms', res.decode('utf-8'))
-            print(system["systemName"] + ": " + ms.group(1) + "ms")
-            
-            thisStatus = '5'   #5=ok, 6=fail:static is fine for now
+            #windows ping: ms = re.search('Average = (.*)ms', res.decode('utf-8'))            
+            ms = re.search('time (.*)ms', res.decode('utf-8'))   
+            thisStatus = '1'   #LOCAL: 5=ok, 6=fail: this is a mess
             thisDuration = ms.group(1)
         
     except subprocess.CalledProcessError as e:        
-        print(system["systemName"] + ": todo - log this error.")
-        thisStatus = '6'
+        thisStatus = '2'
         thisDuration = 0
 
     payload = { 'pw' : secrets.pinglist,
@@ -43,5 +40,3 @@ for system in jsonList["systems"]:
     
     postURL = url + "/responses/" + str(thisSystem)
     res = requests.post(postURL, data=payload)
-    
-    print('***',res.text,'***')
